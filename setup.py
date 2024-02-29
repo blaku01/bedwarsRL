c1c326a133e9bb513c1163a44cff21aad001ff67
blaku01 <gabrielblaczek@gmail.com>
2024-02-29 23:32:06 +0100

    created makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="bedwars AI using Reinforcement Learning",
+    author="Gabriel Blaczek, Przemysław Tomala",
+    author_email="gabrielblaczek@gmail.com, timuslala@gmail.com",
+    url="https://github.com/blaku01/bedwarsRL",
+    install_requires=[],
+    packages=find_packages(),
+    # use this to customize global commands available in the terminal after installing the package
+    # entry_points={
+    #     "console_scripts": [
+    #         "command = src.xyz:main",
+    #     ]
+    # },
+)
